6749d53076bcb72daa3971253981fe20dddf156d
CatiaCorreia <catia.correia97@gmail.com>
2025-08-25 17:53:31 +0100

    Initial setup

---
+++ src/test/java/com/catia/inventory/manager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catia.inventory.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
